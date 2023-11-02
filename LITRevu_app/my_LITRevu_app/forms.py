8efd9800a9db4f4c6c4c9f0b6078295feea01f89
Shinkhan <hzabdelwahid@gmail.com>
2023-11-02 16:00:49 +0100

    updating forms.py meta classes to match what is declared in models.py meta classes

--- LITRevu_app/my_LITRevu_app/forms.py
+++ LITRevu_app/my_LITRevu_app/forms.py
@@ -4,9 +4,29 @@ from .models import Ticket, Review
 class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
-        fields = ['your', 'fields', 'here']
+        fields = ['title', 'description', 'cover']
+        widgets = {
+            'description': forms.Textarea(attrs={'cols': 80, 'rows': 20}),
+        }
+        help_texts = {
+            'title': 'Enter the title of your ticket.',
+        }
+        labels = {
+            'cover': 'Upload a cover image (optional)',
+        }
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['your', 'fields', 'here']
+        fields = ['ticket', 'rating', 'headline', 'body']
+        widgets = {
+            'body': forms.Textarea(attrs={'cols': 80, 'rows': 20}),
+            'rating': forms.NumberInput(attrs={'min': 0, 'max': 5}),
+        }
+        help_texts = {
+            'headline': 'Enter a headline for your review.',
+            'rating': 'Rate the ticket from 0 to 5.',
+        }
+        labels = {
+            'body': 'Write your review here (optional)',
+        }
