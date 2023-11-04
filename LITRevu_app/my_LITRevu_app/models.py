2378f4607cfe1787b27d12ee10a52084273d2272
Shinkhan <hzabdelwahid@gmail.com>
2023-11-04 17:34:04 +0100

    Modification of fields and parameters according to the UML diagram discription of database scheme

--- LITRevu_app/my_LITRevu_app/models.py
+++ LITRevu_app/my_LITRevu_app/models.py
@@ -4,31 +4,32 @@ from django.db import models
 
 
 class Ticket(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    cover = models.ImageField(upload_to='covers/', blank=True, null=True)
+    title = models.CharField(max_length=128)
+    #author = models.CharField(max_length=255)
+    description = models.TextField(max_length=2048, blank=True)
+    #cover = models.ImageField(upload_to='covers/', blank=True, null=True)
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
     time_created = models.DateTimeField(auto_now_add=True)
     pass
 
 
 class Review(models.Model):
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField(
+    rating = models.PositiveSmallIntegerField(max_length=1024,
         # validates that rating must be between 0 and 5
         validators=[MinValueValidator(0), MaxValueValidator(5)])
-    headline = models.CharField(max_length=128)
-    body = models.CharField(max_length=8192, blank=True)
     user = models.ForeignKey(
         to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=128)
+    body = models.TextField(max_length=8192, blank=True)
     time_created = models.DateTimeField(auto_now_add=True)
 
 
 class UserFollows(models.Model):
     # Your UserFollows model definition goes here
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following')
-    followed_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followers')
+    followed_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followed_by')
 
     class Meta:
         # ensures we don't get multiple UserFollows instances
