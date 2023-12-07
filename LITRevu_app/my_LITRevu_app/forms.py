2ec1ce54360d4d054bfed5f5dd3e997e21e0ccb1
Shinkhan <hzabdelwahid@gmail.com>
2023-12-07 15:19:51 +0100

    Adding the class LoginForm and the important imports.

--- LITRevu_app/my_LITRevu_app/forms.py
+++ LITRevu_app/my_LITRevu_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Ticket, Review
-
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
@@ -30,3 +31,8 @@ class ReviewForm(forms.ModelForm):
         labels = {
             'body': 'Write your review here (optional)',
         }
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=63, label="Nom d'utilisateur")
+    password = forms.CharField(max_length=63, label="Mot de passe" ,widget=forms.PasswordInput)
